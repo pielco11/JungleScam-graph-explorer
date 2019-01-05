b732c3814f43288ba15d1a09ea85d96e789909b1
Francesco Poldi <francesco.poldi@protonmail.ch>
2019-01-05 16:25:36 +0100

    Update

--- js/iconType.js
+++ js/iconType.js
@@ -4,5 +4,6 @@ const icons = {
     "Pass" : solid + "pass.svg",
     "Warn" : solid + "warn.svg",
     "Fail" : solid + "fail.svg",
+    "Zero" : solid + "zero.svg",
     "seller" : solid + "seller.svg"
 }
